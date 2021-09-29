e78aaefd1bd10d130af13256028dbec1c7baa2a1
Benjamin Zhuang <zhuanz3@uw.edu>
2021-09-29 08:35:47 -0700

    add resume content

---
+++ resume/src/components/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header>
+
+      </Header>
+
+      <Main>
+
+      </Main>
+
+      <Footer>
+
+      </Footer>
+    </div>
+  );
+}
+
+export default App;
